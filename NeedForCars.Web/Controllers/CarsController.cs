b88b3a8438487eeaed4694230ebc93e9e3561e0c
m1hailp3tkov <41004048+m1hailp3tkov@users.noreply.github.com>
2019-08-04 21:33:26 +0300

    Implemented search, paging and filtering

--- NeedForCars.Web/Controllers/CarsController.cs
+++ NeedForCars.Web/Controllers/CarsController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NeedForCars.Models;
 using NeedForCars.Services.Contracts;
 using NeedForCars.Services.Mapping;
+using NeedForCars.Web.ViewModels.Cars;
 using NeedForCars.Web.ViewModels.UserCars;
 using X.PagedList;
 
@@ -19,16 +22,104 @@ namespace NeedForCars.Web.Controllers
             this.userCarsService = userCarsService;
         }
 
-        public IActionResult Browse(int? page)
+        public IActionResult Browse(int? page, SearchViewModel searchViewModel)
         {
+            var filteredCars = GetUserCarsFiltered(searchViewModel);
+
+            var orderedCars = OrderUserCars(filteredCars, searchViewModel);
+
             int pageNumber = (page ?? 1);
 
-            var allCars = userCarsService
-                .GetAllPublic()
+            var viewModel = orderedCars
                 .To<DisplayUserCarModel>()
                 .ToPagedList(pageNumber, 10);
 
-            return View(allCars);
+            return View(viewModel);
+        }
+
+
+        public IActionResult Search()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Search(SearchViewModel searchViewModel)
+        {
+            return this.RedirectToAction(nameof(Browse), searchViewModel);
+        }
+
+        private IQueryable<UserCar> GetUserCarsFiltered(SearchViewModel model)
+        {
+            var userCars = this.userCarsService.GetAllPublic();
+
+            if (model.MakeId != null) userCars = userCars.Where(x => x.Car.Generation.Model.MakeId == model.MakeId);
+
+            if (model.ModelId != null) userCars = userCars.Where(x => x.Car.Generation.ModelId == model.ModelId);
+
+            if (model.GenerationId != null) userCars = userCars.Where(x => x.Car.GenerationId == model.GenerationId);
+
+            if (model.CarId != null) userCars = userCars.Where(x => x.CarId == model.CarId);
+
+            if (model.MileageFrom != null) userCars = userCars.Where(x => x.Mileage >= model.MileageFrom);
+            if (model.MileageTo != null) userCars = userCars.Where(x => x.Mileage <= model.MileageTo);
+
+            if (model.PriceFrom != null) userCars = userCars.Where(x => x.Price >= model.PriceFrom);
+            if (model.PriceTo != null) userCars = userCars.Where(x => x.Price <= model.PriceTo);
+
+            if (model.YearOfProductionFrom != null) userCars = userCars.Where(x => x.ProductionDateYear >= model.YearOfProductionFrom);
+            if (model.YearOfProductionTo != null) userCars = userCars.Where(x => x.ProductionDateYear <= model.YearOfProductionTo);
+
+            if (model.FuelType != null) userCars = userCars.Where(x => x.Car.Engine.FuelType == model.FuelType);
+
+            if (model.MinHP != null) userCars = userCars.Where(x => x.CustomMaxHP >= model.MinHP || x.Car.Engine.MaxHP >= model.MinHP);
+            if (model.MaxHP != null) userCars = userCars.Where(x => x.CustomMaxHP <= model.MaxHP || x.Car.Engine.MaxHP <= model.MaxHP);
+
+            if (model.Transmission != null) userCars = userCars.Where(x => x.Car.Transmission == model.Transmission);
+
+            if (model.AlternativeFuel != null) userCars = userCars.Where(x => x.AlternativeFuel == model.AlternativeFuel);
+
+            if (model.DriveWheel != null) userCars = userCars.Where(x => x.Car.DriveWheel == model.DriveWheel);
+
+            userCars = OrderUserCars(userCars, model);
+
+            return userCars;
+        }
+
+        private IQueryable<UserCar> OrderUserCars(IQueryable<UserCar> userCars, SearchViewModel model)
+        {
+            switch (model.Ordering)
+            {
+                case Ordering.Latest:
+                    if (model.OrderingType == OrderingType.Ascending)
+                        userCars = userCars.OrderBy(x => x.CreatedOn);
+                    else
+                        userCars = userCars.OrderByDescending(x => x.CreatedOn);
+                    break;
+
+                case Ordering.Mileage:
+                    if (model.OrderingType == OrderingType.Ascending)
+                        userCars = userCars.OrderBy(x => x.Mileage);
+                    else
+                        userCars = userCars.OrderByDescending(x => x.Mileage);
+                    break;
+
+                case Ordering.Price:
+                    if (model.OrderingType == OrderingType.Ascending)
+                        userCars = userCars.OrderBy(x => x.Price);
+                    else
+                        userCars = userCars.OrderByDescending(x => x.Price);
+                    break;
+
+                case Ordering.ProductionDate:
+                    if (model.OrderingType == OrderingType.Ascending)
+                        userCars = userCars.OrderBy(x => new DateTime(x.ProductionDateYear, x.ProductionDateMonth, 1));
+                    else
+                        userCars = userCars.OrderByDescending(x => new DateTime(x.ProductionDateYear, x.ProductionDateMonth, 1));
+                    break;
+            }
+
+            return userCars;
         }
     }
 }
---
+++ NeedForCars.Web/ViewModels/Cars/SearchViewModel.cs
@@ -0,0 +1,63 @@
+﻿using NeedForCars.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeedForCars.Web.ViewModels.Cars
+{
+    public class SearchViewModel
+    {
+        public int? MakeId { get; set; }
+
+        public int? ModelId { get; set; }
+
+        public int? GenerationId { get; set; }
+
+        public int? CarId { get; set; }
+
+        public int? MileageFrom { get; set; }
+
+        public int? MileageTo { get; set; }
+
+        public int? PriceFrom { get; set; }
+
+        public int? PriceTo { get; set; }
+
+        public Currency Currency { get; set; }
+
+        public int? YearOfProductionFrom { get; set; }
+
+        public int? YearOfProductionTo { get; set; }
+
+        public FuelType? FuelType { get; set; }
+
+        public int? MinHP { get; set; }
+
+        public int? MaxHP { get; set; }
+
+        public Transmission? Transmission { get; set; }
+
+        public AlternativeFuel? AlternativeFuel { get; set; }
+
+        public DriveWheel? DriveWheel { get; set; }
+
+        public Ordering Ordering { get; set; }
+
+        public OrderingType OrderingType { get; set; }
+    }
+
+    public enum Ordering
+    {
+        Latest = 1,
+        Price = 2,
+        Mileage = 3,
+        ProductionDate = 4
+    }
+
+    public enum OrderingType
+    {
+        Ascending = 1,
+        Descending = 2
+    }
+}
